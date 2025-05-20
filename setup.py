8e180aad35182cf38246e15f6bba25aaeb81a38f
Mirian Okradze <okradzemirian@gmail.com>
2025-05-21 00:39:11 +0400

    fix: enum imports

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="desearch_py",
-    version="1.0.1",
+    version="1.0.2",
     description="A Python SDK for interacting with the Desearch API service.",
     author="Desearch",
     author_email="",
